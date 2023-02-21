d3e89a504a1f3f349d1ec1504ac9d81c7bacf79f
Gislaine de Aguiar Guimaraes <gis_guimaraes@hotmail.com>
2023-02-21 09:42:12 +0100

    premier commit

---
+++ assets/scripts/card-hover-animation.js
@@ -0,0 +1,6 @@
+// função responsavel por add o listener aos cards
+function addEventListenersToCards() {
+  //constante que vai receber todos os meus cards
+  //cardElements vai retornar um array
+  const cardElements = document.getElementsByClassName('s-card')
+}
